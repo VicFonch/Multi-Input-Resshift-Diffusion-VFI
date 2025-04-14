cadd4abfbc31ed1f4df14158a33cbf068dc07ea4
VicFonch <vmfontech@gmail.com>
2025-04-14 16:23:17 -0600

    interpolate.py: Fix save image function

--- interpolate.py
+++ interpolate.py
@@ -3,7 +3,6 @@ from torchvision.transforms import Compose, ToTensor, Resize, Normalize
 
 import yaml
 from PIL import Image
-import numpy as np
 import matplotlib.pyplot as plt
 
 from model.model import MultiInputResShift
@@ -25,16 +24,16 @@ def parse_args():
 
 def load_image(img_path):
     transforms = Compose([
-        Resize((512, 960)),
+        Resize((256, 448)),
         ToTensor(),
         Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     ])
     return transforms(Image.open(img_path).convert("RGB")).unsqueeze(0).cuda().requires_grad_(False)
 
 def save_image(tensor, path):
-    tensor = denorm(tensor, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    tensor = denorm(tensor.clamp(-1, 1), mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     tensor = tensor.squeeze().permute(1, 2, 0).cpu().numpy()
-    Image.fromarray(tensor).save(path)
+    plt.imsave(path, tensor)
 
 def load_model(confg_path, checkpoint_path):
     with open(confg_path, "r") as f:
@@ -59,7 +58,7 @@ def main():
         save_image(It_list, f"{args.output_path}/It_{i}.png")
     else:
         It = model.reverse_process([img0, img2], args.tau_val)
-        save_image(It, f"{args.output_path}/It.png")
+        save_image(It, f"{args.output_path}/frame2.png")
 
 if __name__ == "__main__":
     main()
